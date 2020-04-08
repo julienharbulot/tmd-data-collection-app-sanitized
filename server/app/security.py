b3db7665813d5eb0ec6efd7838cc3824c032f559
Julien Harbulot <julien.gitcommit@gmail.com>
2020-04-08 12:03:01 +0200

    Import backend server implementation

---
+++ server/app/security.py
@@ -0,0 +1,16 @@
+import os
+from slugify import slugify
+import time
+import hashlib
+
+def make_uid(str):
+    # security protocol not committed into git
+    # implement your own
+    if str == "":
+        str = "unknown"
+    
+    t = f"{time.time()}"
+    s = os.environ["TMD_UID_SALT"]
+    e = (str + s + t).encode('utf-8')
+    h = hashlib.sha1(e).hexdigest()
+    return slugify(h)
